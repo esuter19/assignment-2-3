7ca94523c0ec626059f4d0fb2a256232230042df
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-02 12:11:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+$.getJSON("sf_crime.geojson",function(data){
+   var locations = data.features.map(function(crime) {
+    var location = crime.geometry.coordinates.reverse();
+    location.push(0.5);
+    return location;
+  });
+
+  var heat = L.heatLayer(locations, { radius: 35 });
+  map.addLayer(heat); 
+});
